2de031dee40dcc5bc9abcce731cd8cdb751ad36c
PoppyCurtis <poppy.curtis@gmail.com>
2019-09-29 13:07:15 +0100

    Update local balance with state - bdd

--- lootcheck/src/components/Wallet.js
+++ lootcheck/src/components/Wallet.js
@@ -4,10 +4,22 @@ import balance from '../reducers/balance';
 
 
 export class Wallet extends Component {
+    constructor () {
+        super ();
+        this.state = {
+            balance: undefined
+        }
+    }
+
+    updateBalance = event => {
+        this.setState({ balance: parseInt(event.target.value, 10) });
+    }
     render () {
         return (
             <div>
                 <h3 className='balance'>Wallet balance: {this.props.balance}</h3>
+                <br/>
+                <input className='input-wallet' onChange={this.updateBalance} />
             </div>
         );
     }
--- lootcheck/src/components/Wallet.test.js
+++ lootcheck/src/components/Wallet.test.js
@@ -15,4 +15,22 @@ describe('Wallet', () => {
     it('displays the balance from props', () => {
         expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20');
     });
+    
+    it('creates an input to deposit into or withdraw from the balance', () => {
+        expect(wallet.find('.input-wallet').exists()).toBe(true);
+    });
+
+    describe('when the user types into the wallet component', () => {
+        const userBalance = '25';
+        
+        beforeEach(() => {
+            wallet.find('.input-wallet')
+                .simulate('change', {target: { value: userBalance }});
+        });
+
+        it('updates the local wallet balance in `state` and converts it to a number', () => {
+            //10 = radix to parse into the decimal system
+            expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+        });
+    });
 });
