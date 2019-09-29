5d69174ddae18c3723faf98a0a46a200ec61118d
PoppyCurtis <poppy.curtis@gmail.com>
2019-09-29 11:53:54 +0100

    Setup a store in a react-redux application

--- lootcheck/src/index.js
+++ lootcheck/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import rootReducer from './reducers/balance';
+import { Provider } from 'react-redux';
 import App from  './components/App';
 
-render(<App/>, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, 
+    document.getElementById('root')
+    );
